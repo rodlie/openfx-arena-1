395283a821786935a86b8f9a41cee0a0ce516703
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2020-03-14 18:32:22 +0100

    travis ...

--- .travis.sh
+++ .travis.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
+# MSYS2 wrapper for Travis CI Windows
+
 env
 uname -a
 ls /
 pwd
+ls /mingw64/lib/pkgconfig
+
+export MSYSTEM_CARCH=x86_64
+export MSYSTEM_CHOST=x86_64-w64-mingw32
+export MINGW_PACKAGE_PREFIX=mingw-w64-x86_64
+export MSYSTEM_PREFIX=/mingw64
+export MINGW_CHOST=x86_64-w64-mingw32
+export MSYSTEM=MINGW64
+export MINGW_PREFIX=/mingw64
+export PKG_CONFIG_PATH=/mingw64/lib/pkgconfig:/mingw64/share/pkgconfig
 
 make MINGW=1
