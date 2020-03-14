bd058b7fd39ed9a498a8c1fa9617b5c486e0a5d9
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2020-03-14 19:14:23 +0100

    travis ...........

--- .travis.sh
+++ .travis.sh
@@ -7,13 +7,17 @@ ls /
 pwd
 ls /mingw64/lib/pkgconfig
 
-export MSYSTEM_CARCH=x86_64
-export MSYSTEM_CHOST=x86_64-w64-mingw32
-export MINGW_PACKAGE_PREFIX=mingw-w64-x86_64
-export MSYSTEM_PREFIX=/mingw64
-export MINGW_CHOST=x86_64-w64-mingw32
-export MSYSTEM=MINGW64
-export MINGW_PREFIX=/mingw64
-export PKG_CONFIG_PATH=/mingw64/lib/pkgconfig:/mingw64/share/pkgconfig
+pacman -Syu mingw-w64-x86_64-{cmake,cairo,pango,librsvg2,libzip,poppler,imagemagick,lcms2,librevenge,libcdr,opencolorio-git,sox,libxml2,curl,fontconfig}
+
+ls /mingw64/lib/pkgconfig
+
+#export MSYSTEM_CARCH=x86_64
+#export MSYSTEM_CHOST=x86_64-w64-mingw32
+#export MINGW_PACKAGE_PREFIX=mingw-w64-x86_64
+#export MSYSTEM_PREFIX=/mingw64
+#export MINGW_CHOST=x86_64-w64-mingw32
+#export MSYSTEM=MINGW64
+#export MINGW_PREFIX=/mingw64
+#export PKG_CONFIG_PATH=/mingw64/lib/pkgconfig:/mingw64/share/pkgconfig
 
 make MINGW=1
